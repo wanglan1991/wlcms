9b9e4f299cc60e74c4091d8d4305f4f1bf3e6666
syuenkei <1154853230@qq.com>
2016-04-12 18:49:52 +0800

    04121849

--- src/main/java/com/ekt/cms/shiro/OnLineFilter.java
+++ src/main/java/com/ekt/cms/shiro/OnLineFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.FormAuthenticationFilter;
 
@@ -19,50 +20,57 @@ import com.ekt.cms.index.entity.ResultVO;
 
 public class OnLineFilter extends FormAuthenticationFilter {
 
-    private static final Logger logger = Logger.getLogger(OnLineFilter.class);
+	private static final Logger logger = Logger.getLogger(OnLineFilter.class);
 
-    private String noLoginUrl;
+	private String noLoginUrl;
 
-    public String getNoLoginUrl() {
-        return noLoginUrl;
-    }
+	public String getNoLoginUrl() {
+		return noLoginUrl;
+	}
 
-    public void setNoLoginUrl(String noLoginUrl) {
-        this.noLoginUrl = noLoginUrl;
-    }
+	public void setNoLoginUrl(String noLoginUrl) {
+		this.noLoginUrl = noLoginUrl;
+	}
 
-    @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
-        Subject subject = SecurityUtils.getSubject();
-        if(!subject.isAuthenticated() && subject.isRemembered()){
-        	return true;
-        }
-        
-        if (subject.isAuthenticated()) {
-            return true;
-        }
-        return false;
-    }
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+		Subject subject = SecurityUtils.getSubject();
+		Session session = subject.getSession();
+		if (session != null) {
 
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        HttpServletResponse httpResponse = (HttpServletResponse) response;
-        HttpServletRequest httpRequest = (HttpServletRequest) request;
-        String basePath = ThreadLocalUtil.getBasePath();
-        boolean isAjax = DataUtil.isAjax(httpRequest);
-        if (isAjax) {
-            PrintWriter writer = httpResponse.getWriter();
-            ResultVO resultVO = new ResultVO();
-            resultVO.setOk(false);
-            resultVO.setMsg("请登录");
-            resultVO.setUrl(basePath + getNoLoginUrl());
-            writer.print(resultVO);
-            //writer.print(JSON.toJSONString(resultVO));
-            return false;
-        }
+			if (!subject.isAuthenticated() && subject.isRemembered()) {
+				System.out.println("状态认证");
+				return true;
+			}
 
-        httpResponse.sendRedirect(basePath + getNoLoginUrl());
-        return false;
-    }
+			if (subject.isAuthenticated()) {
+				System.out.println("身份验证");
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	@Override
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+		HttpServletResponse httpResponse = (HttpServletResponse) response;
+		HttpServletRequest httpRequest = (HttpServletRequest) request;
+		String basePath = ThreadLocalUtil.getBasePath();
+		boolean isAjax = DataUtil.isAjax(httpRequest);
+		if (isAjax) {
+			PrintWriter writer = httpResponse.getWriter();
+			ResultVO resultVO = new ResultVO();
+			resultVO.setOk(false);
+			resultVO.setMsg("请登录");
+			resultVO.setUrl(basePath + getNoLoginUrl());
+			writer.print(resultVO);
+			// writer.print(JSON.toJSONString(resultVO));
+			return false;
+		}
+
+		httpResponse.sendRedirect(basePath + getNoLoginUrl());
+		return false;
+	}
 
 }
